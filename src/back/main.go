b61e53c6b5a2f8d606baa6356b9be67bbcb3e514
JoaoRafa19 <joao.rafael@vrsoft.com.br>
2025-03-07 20:06:10 -0300

    generate in main file

--- src/back/main.go
+++ src/back/main.go
@@ -1,10 +1,11 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
 import "github.com/ICEI-PUC-Minas-PCO-SI/2025-1-p3-tiapn-si-grupo-4/cmd"
+
+//go:generate sqlc generate -f ./internal/store/store/sqlc.yml
 
 func main() {
 	cmd.Execute()
